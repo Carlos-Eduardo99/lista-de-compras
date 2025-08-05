a29428f7cbf4786119087fce860b6c3459cca918
Carlos-Eduardo99 <carloscadu1811999@gmail.com>
2025-08-05 12:00:54 -0300

    feat: finalizes shopping list project

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,150 @@
+import { 
+  Image, 
+  TouchableOpacity, 
+  View, 
+  Text, 
+  FlatList, 
+  Alert 
+} from 'react-native';
+
+import { Button } from '@/components/Button';
+import { Input } from '@/components/Input';
+import { Filter } from '@/components/Filter';
+import { Item } from '@/components/Item';
+
+import { FilterStatus } from '@/types/FilterStatus';
+import { styles } from './styles';
+import { useEffect, useState } from 'react';
+import { itemsStorage, ItemStorage } from '@/storage/itemsStorage';
+
+const FILTER_STATUS: FilterStatus[] = [ FilterStatus.PENDING, FilterStatus.DONE]
+
+export function Home() {
+  const [items, setItems] = useState<ItemStorage[]>([])
+  const [filter, setFilter] = useState<FilterStatus>(FilterStatus.PENDING)
+  const [description, setDescription] = useState('')
+
+  const handleAdd = async () => {
+    if(!description.trim()){
+      return Alert.alert("Adicionar", "Informe a descrição para adicionar")
+    }
+
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING
+    }
+
+    await itemsStorage.add(newItem)
+    await itemsByStatus()
+    setDescription('')
+    setFilter(FilterStatus.PENDING)
+
+    Alert.alert("Adicionado", `Adicionado ${description}`)
+  }
+
+  const itemsByStatus = async () => {
+    try {
+      const response = await itemsStorage.getByStatus(filter)
+
+      setItems(response)
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível filtrar os itens.")  
+    }
+  }
+
+  const handleRemove = async (id: string) => {
+    try {
+      await itemsStorage.remove(id)
+      await itemsByStatus()  
+    } catch (error) {
+      Alert.alert("Remover", "Não foi possível remover.")  
+    }
+  }
+
+  const handleClear = () => {
+    Alert.alert("Limpar", "Deseja remover todos?", [
+      {text: "Não", style: "cancel"},
+      {text: "Sim", onPress: () => onClear()}
+    ])
+  }
+  
+  const onClear = async () => {
+    try {
+      await itemsStorage.clear()
+      setItems([]) 
+    } catch (error) {
+      Alert.alert("Erro", 'Não foi possível remover todos os itens.')  
+    }
+  }
+
+  const handleToggleItemStatus = async (id: string) => {
+    try {
+      await itemsStorage.toggleStatus(id)
+      await itemsByStatus()  
+    } catch (error) {
+      Alert.alert("Erro", 'Não foi possível atualizar o status.')    
+    }  
+  }
+
+  useEffect(() => {
+    itemsByStatus()
+  }, [filter])
+
+  return (
+    <View style={styles.container}>
+      <Image 
+        source={require("@/assets/logo.png")} 
+        style={styles.logo} 
+      />
+
+      <View style={styles.form}>
+        <Input 
+          placeholder='O que você precisa comprar?'
+          onChangeText={setDescription}
+          value={description}
+        />
+
+        <Button title='Adicionar' onPress={handleAdd} />
+      </View>
+
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status) => (
+            <Filter 
+              key={status} 
+              status={status} 
+              isActive={status === filter}
+              onPress={() => setFilter(status)}
+            />  
+          ))}
+
+          <TouchableOpacity 
+            style={styles.clearButton} 
+            onPress={handleClear}
+          >
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlatList 
+          data={items} 
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Item
+              data={item} 
+              onRemove={() => handleRemove(item.id)}
+              onStatus={() => handleToggleItemStatus(item.id)}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          contentContainerStyle={styles.listContent}
+          ListEmptyComponent={()=><Text style={styles.empty}>Nenhum item aqui.</Text>}
+        />
+      </View>
+    </View>
+  )
+}
+
+
